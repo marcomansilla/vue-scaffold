b590ab9651a244e138e80d174c358ae3c1733fec
marco <marco@xignus.com.ar>
2018-02-19 04:45:16 -0300

    uploads working

--- static/vue/upload.js
+++ static/vue/upload.js
@@ -23,37 +23,49 @@ var upload=new Vue({
 		});
 	    console.log('posting data');
 	},
-	
+
 	processImg(e){
-	    // get images
 	    this.subidas=[];
 	    console.log('Processing img...');
 	    this.imagen=e.target.files;
-	    var images=e.target.files;
-	    
-	    // encode and post
-	    if (images.length>1){
-		for (i=0;i<images.length;i++){
-		    this.encondeImg(images[i]);
-		    console.log(this.subidas);
-		}
-	    }else{
-		this.encondeImg(images[0]);
-	    }
-	    console.log(this.subidas);
-	},
 
-	encondeImg(file){
-	    var reader = new FileReader();
-	    reader.readAsDataURL(file);
-	    console.log('encoding...');
-	    reader.onload = (file)=> {
-		this.postData('../services/api/simpleupload', {name:file.name, filename:reader.result});
-	    };
+	    var formData = new FormData();
 
-	    reader.onerror = (error)=> {
-		console.log('Error: ', error);
-	    };	       
+	    formData.append('name', this.imagen[0].name);
+	    formData.append('filename', this.imagen[0]);
+	    this.postData('../services/api/simpleupload', formData);
 	}
+	
+	// processImg(e){
+	//     // get images
+	//     this.subidas=[];
+	//     console.log('Processing img...');
+	//     this.imagen=e.target.files;
+	//     var images=e.target.files;
+	    
+	//     // encode and post
+	//     if (images.length>1){
+	// 	for (i=0;i<images.length;i++){
+	// 	    this.encondeImg(images[i]);
+	// 	    console.log(this.subidas);
+	// 	}
+	//     }else{
+	// 	this.encondeImg(images[0]);
+	//     }
+	//     console.log(this.subidas);
+	// },
+
+	// encondeImg(file){
+	//     var reader = new FileReader();
+	//     reader.readAsDataURL(file);
+	//     console.log('encoding...');
+	//     reader.onload = (file)=> {
+	// 	this.postData('../services/api/simpleupload', {name:file.name, filename:reader.result});
+	//     };
+
+	//     reader.onerror = (error)=> {
+	// 	console.log('Error: ', error);
+	//     };	       
+	// }
     }
 });
